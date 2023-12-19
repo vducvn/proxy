a454d386cc61d856bedea24d77b96e68165c2095
vducvn <126306356+vducvn@users.noreply.github.com>
2023-12-19 19:33:08 +0700

    500.sh

--- 500.sh
+++ 500.sh
@@ -89,8 +89,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=62032
-LAST_PORT=62532
+FIRST_PORT=57538
+LAST_PORT=58038
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
